22a258560f70f3f8155e44a9b0d5cf5908365814
acradibotC_tuananh <nguyentuanahh020601@gmail.com>
2023-11-09 13:54:01 +0700

    final core

--- Tabii.Utilities/EmailSender.cs
+++ Tabii.Utilities/EmailSender.cs
@@ -1,6 +1,9 @@
-﻿using Microsoft.AspNetCore.Identity.UI.Services;
+﻿using MailKit.Net.Smtp;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using MimeKit;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,6 +14,20 @@ namespace Tabii.Utilities
     {
         public Task SendEmailAsync(string email, string subject, string htmlMessage)
         {
+            var emailTosend = new MimeMessage();
+            emailTosend.From.Add(MailboxAddress.Parse("Hello@dotnetmastery.com"));
+            emailTosend.To.Add(MailboxAddress.Parse(email));
+            emailTosend.Subject = subject;
+            emailTosend.Body = new TextPart(MimeKit.Text.TextFormat.Html) { Text = htmlMessage };
+
+            //send email
+            using (var emailClient = new SmtpClient())
+            {
+                emailClient.Connect("smtb.gamil.com", 587, MailKit.Security.SecureSocketOptions.StartTls);
+                emailClient.Authenticate("donetmastery@gmail.com", "Donet123$");
+                emailClient.Send(emailTosend);
+                emailClient.Disconnect(true);
+            }
             return Task.CompletedTask;
         }
     }
